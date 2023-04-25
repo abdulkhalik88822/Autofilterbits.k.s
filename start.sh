bbd74c19ae64dbc21932cdf703d0efce3650f1ea
Sanju1t85 <101995551+Sanju1t85@users.noreply.github.com>
2023-04-25 11:46:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sanju1t85/S.K.S.git /Sanju1t85
+  git clone https://github.com/Sanju1t85/S.K.S.git /S.K.S
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TOM-BOT
+  git clone $UPSTREAM_REPO /S.K.S
 fi
-cd /Sanju1t85
+cd /S.K.S
 pip3 install -U -r requirements.txt
 echo "Starting...."
 python3 bot.py
