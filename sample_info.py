de440d0579e0428fd50915c84aafdd9acc42c806
Sanju1t85 <101995551+Sanju1t85@users.noreply.github.com>
2023-04-25 11:42:43 +0530

    2

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 22882517
-API_HASH = 'fdbcfbaf87d91ca8f6cea9badd4f8876'
-BOT_TOKEN = '6251010310:AAEjCFcPjJotlQyaUssYWKtzU9hEkl3AZtM'
+API_ID = 12345
+API_HASH = '0123456789abcdef0123456789abcdef'
+BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,15 +11,15 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [5277442733]
-CHANNELS = [-100119190739]
+ADMINS = [12345789, 'admin123', 98765432]
+CHANNELS = [-10012345678, -100987654321, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 LOGIN_CHANNEL = None
 
 # MongoDB information
 DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'cluster0'
-COLLECTION_NAME = 'Telegram_files'  # If you are using the same database, then use different collection name for each bot
+DATABASE_NAME = 'Telegram'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 
